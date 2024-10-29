e49567ba3efa3e0849b0048f5454b2a8445e7da3
novouss <raymondgorospe1113@gmail.com>
2024-10-29 23:47:36 +0800

    Added audio record and evironment configuration

--- scripts/replayRecord.sh
+++ scripts/replayRecord.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+pactl load-module module-echo-cancel
+pactl set-default-source echo-cancel-source
+
 pidof -q gpu-screen-recorder && exit 0
-video_path="$HOME/Videos"
-gpu-screen-recorder -w screen -f 60 -a default_output -c mp4 -r 120 -o "$video_path"
+video_path="$HOME/Videos/Replays"
+gpu-screen-recorder -w screen -f 60 -a "$(pactl get-default-sink).monitor|$(pactl get-default-source)" -c mp4 -r 120 -o "$video_path"
