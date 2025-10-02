033cc83340c7f869c6fe39b68bb04d888c4ebc41
novouss <raymondgorospe1113@gmail.com>
2025-10-03 01:50:31 +0800

    fix(ignis): updated function names from old documentation

--- .config/ignis/config.py
+++ .config/ignis/config.py
@@ -1,10 +1,12 @@
 import os
 import datetime
 
-from ignis import widgets
-from ignis import utils
+from ignis.widgets import Widget
+from ignis.utils import Utils
 
-from ignis.css_manager import CssManager, CssInfoPath
+# from ignis.css_manager import CssManager, CssInfoPath
+
+from ignis.app import IgnisApp
 
 from ignis.services.audio import AudioService
 from ignis.services.hyprland import HyprlandService, HyprlandWorkspace
@@ -12,26 +14,29 @@ from ignis.services.system_tray import SystemTrayService, SystemTrayItem
 
 from scripts import get_weather
 
-css_manager = CssManager.get_default()
+# css_manager = CssManager.get_default()
+#
+# css_manager.apply_css(
+#     CssInfoPath(
+#         name="main",
+#         compiler_function=lambda path: Utils.sass_compile(path=path),
+#         path=os.path.join(Utils.get_current_dir(), "style.scss"),
+#     )
+# )
 
-css_manager.apply_css(
-    CssInfoPath(
-        name="main",
-        compiler_function=lambda path: utils.sass_compile(path=path),
-        path=os.path.join(utils.get_current_dir(), "style.scss"),
-    )
-)
+app = IgnisApp.get_default()
+app.apply_css(os.path.expanduser("~/dotfiles/.config/ignis/style.scss"))
 
 audio = AudioService.get_default()
 hyprland = HyprlandService.get_default()
 systemtray = SystemTrayService.get_default()
 
 
-def workspace_button(workspace: HyprlandWorkspace) -> widgets.Button:
-    widget = widgets.Button(
+def workspace_button(workspace: HyprlandWorkspace) -> Widget.Button:
+    widget = Widget.Button(
         css_classes=["workspace-button"],
         on_click=lambda _: workspace.switch_to(),
-        child=widgets.Label(label=str(workspace.id)),
+        child=Widget.Label(label=str(workspace.id)),
     )
     if workspace.id == hyprland.active_workspace.id:
         widget.add_css_class("active")
@@ -40,8 +45,8 @@ def workspace_button(workspace: HyprlandWorkspace) -> widgets.Button:
     return widget
 
 
-def workspaces() -> widgets.EventBox:
-    return widgets.EventBox(
+def workspaces() -> Widget.EventBox:
+    return Widget.EventBox(
         css_classes=["workspaces"],
         spacing=5,
         child=hyprland.bind_many(
@@ -53,8 +58,8 @@ def workspaces() -> widgets.EventBox:
     )
 
 
-def client_title() -> widgets.Label:
-    return widgets.Label(
+def client_title() -> Widget.Label:
+    return Widget.Label(
         css_classes=["active-title"],
         ellipsize="end",
         max_width_chars=40,
@@ -66,39 +71,39 @@ def datetime_format(frmt: str) -> str:
     return datetime.datetime.now().strftime(frmt)
 
 
-def clock() -> widgets.Label:
-    return widgets.Label(
-        label=utils.Poll(
+def clock() -> Widget.Label:
+    return Widget.Label(
+        label=Utils.Poll(
             1000,
             lambda _: datetime_format("  %I:%M %p")
         ).bind("output")
     )
 
 
-def calendar() -> widgets.Label:
-    return widgets.Label(
-        label=utils.Poll(
+def calendar() -> Widget.Label:
+    return Widget.Label(
+        label=Utils.Poll(
             3600000,
             lambda _: datetime_format("  %a %e")
         ).bind("output")
     )
 
 
-def separator() -> widgets.Separator:
-    return widgets.Separator(
+def separator() -> Widget.Separator:
+    return Widget.Separator(
         vertical=True,
         css_classes=["middle-separator"]
     )
 
 
-def volume() -> widgets.Box:
-    return widgets.Box(
+def volume() -> Widget.Box:
+    return Widget.Box(
         child=[
-            widgets.Icon(
+            Widget.Icon(
                 image=audio.speaker.bind("icon_name"),
                 style="margin-right: 5px;"
             ),
-            widgets.Label(
+            Widget.Label(
                 label=audio.speaker.bind(
                     "volume",
                     transform=lambda value: str(value)
@@ -108,15 +113,15 @@ def volume() -> widgets.Box:
     )
 
 
-def tray_item(item: SystemTrayItem) -> widgets.Button:
+def tray_item(item: SystemTrayItem) -> Widget.Button:
     if item.menu:
         menu = item.menu.copy()
     else:
         menu = None
-    return widgets.Button(
-        child=widgets.Box(
+    return Widget.Button(
+        child=Widget.Box(
             child=[
-                widgets.Icon(
+                Widget.Icon(
                     image=item.bind("icon"),
                     pixel_size=18
                 ),
@@ -132,7 +137,7 @@ def tray_item(item: SystemTrayItem) -> widgets.Button:
 
 
 def tray():
-    return widgets.Box(
+    return Widget.Box(
         setup=lambda self: systemtray.connect(
             "added", lambda _, item: self.append(tray_item(item))
         ),
@@ -140,17 +145,17 @@ def tray():
     )
 
 
-def weather() -> widgets.Label:
-    return widgets.Label(
-        label=utils.Poll(
+def weather() -> Widget.Label:
+    return Widget.Label(
+        label=Utils.Poll(
             3_600_000,
             lambda _: get_weather()
         ).bind("output")
     )
 
 
-def modules_left() -> widgets.Box:
-    return widgets.Box(
+def modules_left() -> Widget.Box:
+    return Widget.Box(
         css_classes=["modules-left"],
         child=[
             clock(),
@@ -162,16 +167,16 @@ def modules_left() -> widgets.Box:
     )
 
 
-def modules_center() -> widgets.Box:
-    return widgets.Box(
+def modules_center() -> Widget.Box:
+    return Widget.Box(
         child=[
             workspaces(),
         ],
     )
 
 
-def modules_right() -> widgets.Box:
-    return widgets.Box(
+def modules_right() -> Widget.Box:
+    return Widget.Box(
         css_classes=["modules-right"],
         child=[
             tray(),
@@ -183,14 +188,14 @@ def modules_right() -> widgets.Box:
     )
 
 
-def bar(monitor_id: int = 0) -> widgets.Window:
-    return widgets.Window(
+def bar(monitor_id: int = 0) -> Widget.Window:
+    return Widget.Window(
         namespace=f"ignis_bar_{monitor_id}",
         css_classes=["bar"],
         monitor=monitor_id,
         anchor=["left", "top", "right"],
         exclusivity="exclusive",
-        child=widgets.CenterBox(
+        child=Widget.CenterBox(
             start_widget=modules_left(),
             center_widget=modules_center(),
             end_widget=modules_right()
@@ -198,5 +203,5 @@ def bar(monitor_id: int = 0) -> widgets.Window:
     )
 
 
-for i in range(utils.get_n_monitors()):
+for i in range(Utils.get_n_monitors()):
     bar(i)
