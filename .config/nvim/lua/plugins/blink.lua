003c540c2a9662ca0903ca63499bc75563da6439
novouss <raymondgorospe1113@gmail.com>
2026-06-21 07:43:33 +0800

    feat(nvim): added recommended configuration for blink

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,23 +1,57 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
-  version = '1.*',
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = { preset = 'default' },
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
-    completion = { 
-      documentation = { 
-        auto_show = false 
-      } 
-    },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  opts_extend = { "sources.default" }
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
+	version = "1.*",
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		keymap = { preset = "default" },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+		completion = {
+			menu = {
+				draw = {
+					components = {
+						kind_icon = {
+							text = function(ctx)
+								local icon = ctx.kind_icon
+								if ctx.item.source_name == "LSP" then
+									local color_item = require("nvim-highlight-colors").format(
+										ctx.item.documentation,
+										{ kind = ctx.kind }
+									)
+									if color_item and color_item.abbr ~= "" then
+										icon = color_item.abbr
+									end
+								end
+								return icon .. ctx.icon_gap
+							end,
+							highlight = function(ctx)
+								local highlight = "BlinkCmpKind" .. ctx.kind
+								if ctx.item.source_name == "LSP" then
+									local color_item = require("nvim-highlight-colors").format(
+										ctx.item.documentation,
+										{ kind = ctx.kind }
+									)
+									if color_item and color_item.abbr_hl_group then
+										highlight = color_item.abbr_hl_group
+									end
+								end
+								return highlight
+							end,
+						},
+					},
+				},
+			},
+			documentation = {
+				auto_show = false,
+			},
+		},
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+	},
+	opts_extend = { "sources.default" },
 }
